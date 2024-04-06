e138b9f56b3dec18f9ca2ab278b7ffdb5195faad
metucker <70479885+metucker@users.noreply.github.com>
2024-04-06 15:32:49 -0400

    add content to Browse page for testing

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -1,9 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import withAuthentication from '../components/Authentication/withAuthentication';
+import CatList from '../components/CatList';
+
+
 function Browse() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    // Fetch cats from the backend API
+    fetch('/cats')
+      .then(response => response.json())
+      .then(data => setCats(data))
+      .catch(error => console.error('Error fetching cats:', error));
+  }, []);
+
   return (
-    <div>
-      <h1>Browse</h1>
-    </div>
+    <>
+      <h1>Browse Cats Near You</h1>
+      <div className="cat-page">
+        <CatList cats={cats} />
+      </div>
+    </>
   );
 }
 
-export default Browse
+export default withAuthentication(Browse)
