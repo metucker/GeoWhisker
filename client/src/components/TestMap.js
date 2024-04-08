a4425e55058c5a60986be649c9c6de3fb8c294c7
metucker <70479885+metucker@users.noreply.github.com>
2024-04-07 23:03:36 -0400

    test maps

--- client/src/components/TestMap.js
+++ client/src/components/TestMap.js
@@ -87,3 +87,70 @@ const TestMap = ({setPolygonCoordinates}) => {
 };
 
 export default TestMap;
+
+// import React from 'react';
+// import { GoogleMap, useJsApiLoader, DrawingManager } from '@react-google-maps/api';
+
+// const libraries = ['places', 'drawing'];
+// const mapContainerStyle = {
+//   width: '100%',
+//   height: '400px',
+//   maxWidth: '800px'
+// };
+
+// const center = {
+//   lat: 29.6516,
+//   lng: -82.3248
+// };
+
+// const TestMap = ({ setPolygonCoordinates }) => {
+//   const { isLoaded, loadError } = useJsApiLoader({
+//     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+//     libraries,
+//   });
+
+//   const onPolygonComplete = (polygon) => {
+//     const polygonCoordinates = polygon.getPath().getArray().map(latLng => ({
+//       lat: latLng.lat(),
+//       lng: latLng.lng()
+//     }));
+//     console.log("POLYGON COORDINATES:", polygonCoordinates);
+//     setPolygonCoordinates(polygonCoordinates);
+//   };
+
+//   return (
+//     <>
+//       {loadError && <div>Error loading maps</div>}
+//       {!isLoaded && <div>Loading maps</div>}
+//       {isLoaded && (
+//         <GoogleMap
+//           mapContainerStyle={mapContainerStyle}
+//           zoom={15}
+//           center={center}
+//         >
+//           <DrawingManager
+//             drawingControlOptions={{
+//               position: window.google.maps.ControlPosition.TOP_CENTER,
+//               drawingModes: [window.google.maps.drawing.OverlayType.POLYGON],
+//             }}
+//             options={{
+//               drawingControl: true,
+//               drawingMode: window.google.maps.drawing.OverlayType.POLYGON,
+//               polygonOptions: {
+//                 fillColor: '#3f7c7c',
+//                 fillOpacity: 0.5,
+//                 strokeWeight: 5,
+//                 clickable: true,
+//                 editable: true,
+//                 zIndex: 1
+//               }
+//             }}
+//             onPolygonComplete={onPolygonComplete}
+//           />
+//         </GoogleMap>
+//       )}
+//     </>
+//   );
+// };
+
+// export default TestMap;
